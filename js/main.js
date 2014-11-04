e7e8ecd432ff6941ebbe552e0e3d3c413cd57c92
anpham <t3phho00@students.oamk.fi>
2014-11-04 18:04:09 +0200

    basic template for main page and others

--- js/main.js
+++ js/main.js
@@ -14,53 +14,52 @@ app.controller("HeaderController", function($scope, $http) {
 
 // navigation 
 app.controller("NavigationController", function($scope, $http) {
-    $scope.tabs = [
-        {name: "Activity", link: "#", active: false},
-        {name: "Tasks", link: "tasks", active: false},
-        {name: "Members", link: "members", active: false},
-        {name: "Notes", link: "notes", active: false}
+    $scope.navs = [
+        {link: "project_name", name: "Acitivity", active: false},
+        {link: "project_name/tasks", name: "Tasks", active: false},
+        {link: "project_name/notes", name: "Notes", active: false},
+        {link: "project_name/edit", name: "Edit Project", active: false},
     ];
 
     $scope.set_route = function(id) {
-        console.log(id);
-        for (var i = 0; i < $scope.tabs.length(); i++) {
-            $scope.tabs[i].active = false;
+        for (index = 0; index < $scope.navs.length; ++index) {
+            $scope.navs[index].active = false;
         }
-        $scope.tabs[id].active = true;
-        $scope.tab = $scope.tabs[id];
+        $scope.navs[id].active = true;
+
+        $scope.nav = $scope.navs[id];
     };
 });
 
 // home page
 app.controller("HomeController", function($scope, $http) {
-    $scope.projects = [
-    {id: "1", code: "MHK01", name: "Project 1", description: "A soft description about project", duedate: "01.01.2015", status: "Active", leader: "Mr.A"},
-    {id: "2", code: "MHK02", name: "Project 2", description: "A soft description about project", duedate: "01.01.2015", status: "New", leader: "Mr.B"},
-    {id: "3", code: "MHK03", name: "Project 3", description: "A soft description about project", duedate: "01.01.2015", status: "Finished", leader: "Mr.A"},
-    {id: "4", code: "MHK04", name: "Project 4", description: "A soft description about project", duedate: "01.01.2015", status: "Active", leader: "Mr.B"}
-    ];
+    $scope.projects = allProject;
     $('.l-sidebar').addClass("animated fadeInLeft");
+    $('#navigation').css("display","none");
 });
 
 // main page
 app.controller("MainController", function($scope, $http) {
+    $('#navigation').css("display","block");
     angular.element($("#navigation")).scope().set_route(0);
 });
 
 // tasks page
 app.controller("TasksController", function($scope, $http) {
-
+    $('#navigation').css("display","block");
     angular.element($("#navigation")).scope().set_route(1);
 });
 
 // members page
-app.controller("MembersController", function($scope, $http) {
+app.controller("NotesController", function($scope, $http) {
+    $('#navigation').css("display","block");
     angular.element($("#navigation")).scope().set_route(2);
 });
 
 // notes page
-app.controller("NotesController", function($scope, $http) {
-    angular.element($("#navigation")).scope().set_route(3);
+app.controller("EditController", function($scope, $http) {
+   $('#navigation').css("display","block"); 
+   angular.element($("#navigation")).scope().set_route(3);
 });
 
 // route setting
@@ -80,15 +79,15 @@ app.config(function($routeProvider, $locationProvider) {
             templateUrl : 'template/tasks.html',
             controller  : 'TasksController'
         })
-        .when('/project_name/members', {
-            templateUrl : 'template/members.html',
-            controller  : 'MembersController'
-        })
         .when('/project_name/notes', {
             templateUrl : 'template/notes.html',
             controller  : 'NotesController'
         })
-
+        .when('/project_name/edit', {
+            templateUrl : 'template/edit_project.html',
+            controller  : 'EditController'
+        })
+        
         // redirect if route not found
         .otherwise({
             redirectTo: '/'
@@ -106,3 +105,11 @@ $(function() {
 $(window).resize(function() {
     $(".content.right").css("width", $(window).width() - 320);
 });
+
+// database
+var allProject = [
+    {id: "1", code: "MHK01", name: "Project 1", description: "A short description about project", duedate: "01.01.2015", status: "Active", leader: "Mr.A"},
+    {id: "2", code: "MHK02", name: "Project 2", description: "A short description about project", duedate: "01.01.2015", status: "New", leader: "Mr.B"},
+    {id: "3", code: "MHK03", name: "Project 3", description: "A short description about project", duedate: "01.01.2015", status: "Finished", leader: "Mr.A"},
+    {id: "4", code: "MHK04", name: "Project 4", description: "A short description about project", duedate: "01.01.2015", status: "Active", leader: "Mr.B"}
+    ];
